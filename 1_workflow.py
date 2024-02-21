27e1b0c59a67e4be8e3ea1a22fe82d3bc798a05d
Eduardo Q <qe.eduardo99@gmail.com>
2024-02-21 15:35:24 -0600

    feat: train model in workflow module

--- 1_workflow.py
+++ 1_workflow.py
@@ -43,3 +43,44 @@ print(model_0.state_dict())
 
 with torch.inference_mode(): 
     y_preds = model_0(X_test)
+
+   
+# -- TRAIN MODEL --
+loss_fn = nn.L1Loss() # MAE loss is same as L1Loss
+optimizer = torch.optim.SGD(params=model_0.parameters(), lr=0.01) 
+
+# Training loop
+# 1. Forward pass
+# 2. Calculate loss
+# 3. Zero gradients
+# 4. Backward pass
+# 5. Optimizer step
+
+# Evaluate
+# 1. Forward pass
+# 2. Calculate loss
+
+epochs = 100
+train_loss_values = []
+test_loss_values = []
+epoch_count = []
+
+for epoch in range(epochs):
+    model_0.train()
+
+    y_pred = model_0(X_train) # 1
+    loss = loss_fn(y_pred, y_train) # 2
+    optimizer.zero_grad()# 3
+    loss.backward() # 4
+    optimizer.step() # 5
+
+    model_0.eval()
+    with torch.inference_mode():
+      test_pred = model_0(X_test) # 1
+      test_loss = loss_fn(test_pred, y_test.type(torch.float)) # 2 
+
+      if epoch % 10 == 0:
+            epoch_count.append(epoch)
+            train_loss_values.append(loss.detach().numpy())
+            test_loss_values.append(test_loss.detach().numpy())
+            print(f"Epoch: {epoch} | MAE Train Loss: {loss} | MAE Test Loss: {test_loss} ")
