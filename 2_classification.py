bd97d626be509af1b88e688c3ac6ead58f8b8562
Eduardo Q <qe.eduardo99@gmail.com>
2024-02-26 14:40:14 -0600

    feat: add model on clss module

--- 2_classification.py
+++ 2_classification.py
@@ -22,3 +22,33 @@ X = torch.from_numpy(X).type(torch.float)
 y = torch.from_numpy(y).type(torch.float)
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42) 
+
+# -- MODEL --
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+class CircleModelV0(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.layer_1 = nn.Linear(in_features=2, out_features=5)
+        self.layer_2 = nn.Linear(in_features=5, out_features=1)
+    
+    def forward(self, x):
+        return self.layer_2(self.layer_1(x))
+
+model_0 = CircleModelV0().to(device)
+
+# Replicate CircleModelV0 with nn.Sequential
+model_0 = nn.Sequential(
+    nn.Linear(in_features=2, out_features=5),
+    nn.Linear(in_features=5, out_features=1)
+).to(device)
+
+untrained_preds = model_0(X_test.to(device))
+
+loss_fn = nn.BCEWithLogitsLoss()
+optimizer = torch.optim.SGD(params=model_0.parameters(), lr=0.1)
+
+def accuracy_fn(y_true, y_pred):
+    correct = torch.eq(y_true, y_pred).sum().item()
+    acc = (correct / len(y_pred)) * 100 
+    return acc
